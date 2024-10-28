60f31ab2418288499d3a8bcb4651e07fb48a7f6e
Luisa Stellet <luisastellet@id.uff.br>
2024-10-28 10:26:18 -0300

    ajeitando a resposividade, com media query

--- index.php
+++ index.php
@@ -25,13 +25,13 @@
     <main>
         <section id="capa">
             <h1>Luisa Stellet</h1>
-            <h3>Graduanda em Ciência da Computação</h3>
-            <p>Programa Estagiar Globo 2024</p>
+            <h3 id="atencao">Graduanda em Ciência da Computação</h3>
+            <p class="texto-maior" id="estagio">Programa Estagiar Globo 2024</p>
         </section> 
 
         <section id="sobremim">
-            <div class="box">
-                <p>Aluna do 4° período de <strong>Ciência da Computação</strong> na Universidade Federal Fluminense - UFF</p>
+            <div class="box" >
+                <p class="texto-maior">Aluna do 4º período de Ciência da Computação na Universidade Federal Fluminense (UFF).</p>
                 <p>2023 - 2026 (Previsão de formatura)</p>
             </div>
                 <figure>
@@ -42,13 +42,12 @@
         </section class="sobremim">
 
         <section class="experiencias">
-            <h3>Experiências valiosas :</h3>
+            <h3>Experiências que trouxeram aprendizados sobre conviver com a diferença :</h3>
             <div class="box">
-                <p>Participei durante 1 ano e meio do projeto de extensão Incluir que visa promover a inclusão digital de idosos. Sou voluntária do projeto Sempre Criança, que ajuda crianças e familias em situação de vulnerabilidade. Entrei no projeto em 2020 e continuo até hoje!</p>
+                <p>Participei durante um ano e meio do projeto de extensão Incluir, que visa promover a inclusão digital de idosos. Sou voluntária do projeto Sempre Criança, que ajuda crianças e famílias em situação de vulnerabilidade. Entrei no projeto em 2020 e continuo até hoje!</p>
             </div>
 
             <figure>
-                <img src="fotos/6.jpg" alt="Incluir">
                 <img src="fotos/7.jpg" alt="Incluir">
                 <img src="fotos/12.jpg" alt="Sempre criança">
                 <img src="fotos/2.jpg" alt="Sempre criança">
@@ -56,9 +55,9 @@
         </section>
 
         <section class="experiencias">
-            <h3>Experiências valiosas :</h3>
+            <h3>Experiências que trouxeram aprendizados sobre conviver com a diferença :</h3>
             <div class="box">
-                <p>Em setembro desse ano fiz a minha primeira viagem sozinha para um Congresso para apresentar um artigo que foi escrito em cima do projeto de web que eu fiz enquanto era bolsista do grupo PET-Tele da UFF.</p>
+                <p>Em setembro deste ano, fiz minha primeira viagem sozinha para um congresso, onde apresentei um artigo que foi escrito com base no projeto de web que desenvolvi enquanto era bolsista do grupo PET-Tele da UFF.</p>
             </div>
 
             <figure>
@@ -69,9 +68,21 @@
         </section>
 
         <section id="respostas">
-            <div class="box">
-                <h3>Respondendo às perguntas: </h3>
-            </div>
+            <h3>Respondendo às perguntas: </h3>
+            <div class="div-respostas">
+                <div class="box">
+                    <h5>Como você acredita que participar do Programa terá um papel importante na sua história profissional?</h5>
+                    <hr>
+                    <p>Estagiando na Globo, tenho a oportunidade de adquirir novas habilidades e conhecimentos com pessoas mais experientes, expandir minha rede de contatos e conviver em um ambiente acolhedor, colaborativo e diverso. Com isso, conseguirei enriquecer minha formação acadêmica e me destacar na minha área.</p>
+                </div>
+
+                <div class="box">
+                    <h5>O que você acredita que seja sua maior contribuição para 
+                    sua liderança e colegas de trabalho?</h5>
+                    <hr>
+                    <p>Acredito que minha maior contribuição seja a comunicação assertiva, que garante um ambiente de trabalho tranquilo e harmônico. Busco um lugar onde possa compartilhar minhas ideias, pensamentos e inovações de forma acolhedora e livre de julgamentos. Sempre proponho aos meus colegas um diálogo aberto, com trocas de ideias e uma comunicação inclusiva. Assim, temos uma equipe mais unida, onde todos se sentem ouvidos e motivados a colaborar e contribuir.</p>
+                </div>
+                </div>
 
         </section>
 
@@ -80,15 +91,15 @@
             <div id="coluna">
                 <div id="contact_me">
                     <div style="text-align: center;">
-                        <a href="linkedin.com/in/luisastellet" target="_blank">
+                        <a href="https://linkedin.com/in/luisastellet" target="_blank">
                             <img src="fotos/linkedin.png" alt="Linkedin">
-                            <p style="color: var(--azul); font-weight: bold;">Se conecte comigo!</p>
+                            <p style="color: var(--roxo-azulado); font-weight: bold;">Se conecte comigo!</p>
                         </a>
                     </div>
                     <div style="text-align: center;">
                         <a href="https://github.com/luisastellet" target="_blank">
                             <img src="fotos/github.png" alt="Github">
-                            <p style="color: var(--azul); font-weight: bold;">Veja meu portfólio!</p>
+                            <p style="color: var(--roxo-azulado); font-weight: bold;">Veja meu portfólio!</p>
                         </a>
                     </div>
                 </div>
